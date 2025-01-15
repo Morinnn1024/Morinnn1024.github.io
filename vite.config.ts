e9051b4b9dfb51bf804de2f0598de412708a13cf
陌凜. <1349639383@qq.com>
2025-01-15 20:41:11 +0800

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/Morinnn1024.github.io" // production
-      : "/", // development
+  base: '/',
 })
