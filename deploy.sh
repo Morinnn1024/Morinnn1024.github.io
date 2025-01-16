68d1feeed3002a19d1f133ee2fab09ce10612401
陌凜. <1349639383@qq.com>
2025-01-16 17:06:24 +0800

    fix: 更新部署脚本, 解决404问题

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+# 构建项目
+npm run build
+# 复制dist文件夹的`index.html`文件，将复制后的文件重命名为`404.html`
+cp dist/index.html dist/404.html
+# 将项目提交到 main 分支
 git add .
-git commit -m "Deploy to Github Pages" 
+git commit -m "Deploy to Github Pages"
 git push origin main
+# subtree push 到分支 gh-pages
 git subtree push --prefix dist origin gh-pages
